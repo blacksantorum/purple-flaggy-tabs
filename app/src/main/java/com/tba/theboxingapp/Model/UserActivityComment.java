8e625894ef54f1cdcf190ab66bf61d621077f2c9
Chris Tibbs <chris@concreteinteractive.com>
2014-12-02 18:38:28 -0800

    All model

---
+++ app/src/main/java/com/tba/theboxingapp/Model/UserActivityComment.java
@@ -0,0 +1,51 @@
+package com.tba.theboxingapp.Model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * Created by christibbs on 9/28/14.
+ */
+public class UserActivityComment extends BaseModel {
+    public String getFightTitle() {
+        return fightTitle;
+    }
+
+    public void setFightTitle(String fightTitle) {
+        this.fightTitle = fightTitle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    String fightTitle;
+    String content;
+
+    public UserActivityComment (JSONObject object)
+    {
+        try {
+            this.content = object.getString("body");
+            this.fightTitle = object.getString("fight_name");
+            String createdAtDateString = object.getString("created_at");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            sdf.setTimeZone(Calendar.getInstance().getTimeZone());
+            try {
+                this.createdAt = sdf.parse(createdAtDateString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/tba/theboxingapp/Model/UserActivityPrediction.java
@@ -0,0 +1,51 @@
+package com.tba.theboxingapp.Model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * Created by christibbs on 9/28/14.
+ */
+public class UserActivityPrediction extends BaseModel {
+    public String getBoxerImageUrl() {
+        return boxerImageUrl;
+    }
+
+    public void setBoxerImageUrl(String boxerImageUrl) {
+        this.boxerImageUrl = boxerImageUrl;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    String boxerImageUrl;
+    String content;
+
+    public UserActivityPrediction(JSONObject object)
+    {
+        try {
+            this.boxerImageUrl = object.getString("img_winner");
+            this.content = object.getString("text");
+            String createdAtDateString = object.getString("created_at");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            sdf.setTimeZone(Calendar.getInstance().getTimeZone());
+            try {
+                this.createdAt = sdf.parse(createdAtDateString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
